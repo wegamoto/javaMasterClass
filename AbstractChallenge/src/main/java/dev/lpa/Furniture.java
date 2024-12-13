ed0d0bef4352c9052cd467f43dbbb48dd1f5ad5d
Taweewattana Prhomkotara <promakotara@gmail.com>
2024-12-13 15:49:29 +0700

    Daily Updated

---
+++ AbstractChallenge/src/main/java/dev/lpa/Furniture.java
@@ -0,0 +1,17 @@
+package dev.lpa;
+
+public class Furniture extends  ProductForSale {
+
+
+
+    public Furniture(String type, double price, String description) {
+        super(type, price, description);
+    }
+
+    @Override
+    public void showDetails() {
+        System.out.println("This " + type + " was manufatured in North Carolina");
+        System.out.printf("The price of the piece is $ 6.2f %n", price);
+        System.out.println(description);
+    }
+}
