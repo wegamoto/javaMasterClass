bde244e0f0ffaf5504358917b30166519d4e8f7a
Taweewattana Prhomkotara <promakotara@gmail.com>
2025-01-04 15:19:45 +0700

    Daily Updates

---
+++ EnumCollections/src/main/java/dev/lpa/Main.java
@@ -0,0 +1,47 @@
+package dev.lpa;
+
+import java.util.*;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+
+    enum WeekDay {SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY}
+
+    public static void main(String[] args) {
+
+        List<WeekDay> annsWorkDays = new ArrayList<>(List.of(WeekDay.MONDAY,
+                WeekDay.TUESDAY, WeekDay.THURSDAY, WeekDay.FRIDAY));
+
+        var annsDaysSet = EnumSet.copyOf(annsWorkDays);
+        System.out.println(annsDaysSet.getClass().getSimpleName());
+        annsDaysSet.forEach(System.out::println);
+
+        var allDaysSet = EnumSet.allOf(WeekDay.class);
+        System.out.println("-------------------------------------------");
+        allDaysSet.forEach(System.out::println);
+
+        Set<WeekDay> newPersonDays = EnumSet.complementOf(annsDaysSet);
+        System.out.println("-------------------------------------------");
+        newPersonDays.forEach(System.out::println);
+
+        Set<WeekDay> anotherWay = EnumSet.copyOf(allDaysSet);
+        anotherWay.retainAll(annsDaysSet);
+        System.out.println("-------------------------------------------");
+        anotherWay.forEach(System.out::println);
+
+        Set<WeekDay> businessDays = EnumSet.range(WeekDay.MONDAY, WeekDay.FRIDAY);
+        System.out.println("--------------------------------------------");
+        businessDays.forEach(System.out::println);
+
+        Map<WeekDay, String[]> employeeMap = new EnumMap<>(WeekDay.class);
+
+        employeeMap.put(WeekDay.FRIDAY, new String[]{"Ann", "Mary", "Bob"});
+        employeeMap.put(WeekDay.MONDAY, new String[]{"Mary", "Bob"});
+        employeeMap.forEach(
+                (k, v) -> System.out.println(k + " : " + Arrays.toString(v))
+        );
+
+
+    }
+}
