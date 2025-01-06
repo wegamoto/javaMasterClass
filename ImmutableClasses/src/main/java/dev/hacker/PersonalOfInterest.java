fd0978e01e1a1b4a2a635176eb526cc800df3449
Taweewattana Prhomkotara <promakotara@gmail.com>
2025-01-06 15:33:39 +0700

    Daily Updates

---
+++ ImmutableClasses/src/main/java/dev/hacker/PersonalOfInterest.java
@@ -0,0 +1,12 @@
+package dev.hacker;
+
+import dev.lpa.PersonImmutable;
+
+public class PersonalOfInterest extends PersonImmutable {
+
+    public PersonalOfInterest(PersonImmutable person) {
+        super(person);
+    }
+
+    
+}
