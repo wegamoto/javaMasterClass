a4522f05d95dbe56c75f58b6d35955f7ea377970
Taweewattana Prhomkotara <promakotara@gmail.com>
2025-01-11 10:37:07 +0700

    Daily updates

--- Streams/src/dev/lpa/Main.java
+++ Streams/src/dev/lpa/Main.java
@@ -1,9 +1,8 @@
 package dev.lpa;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class Main {
 
@@ -11,9 +10,9 @@ public class Main {
 
         List<String> bingoPool = new ArrayList<>(75);
 
-        int start =1;
-        for(char c : "BINGO".toCharArray()) {
-            for(int i = start; i< (start + 15); i++) {
+        int start = 1;
+        for (char c : "BINGO".toCharArray()) {
+            for (int i = start; i < (start + 15); i++) {
                 bingoPool.add("" + c + i);
 //                System.out.println("" + c + i);
             }
@@ -24,10 +23,10 @@ public class Main {
         for (int i = 0; i < 15; i++) {
             System.out.println(bingoPool.get(i));
         }
-        System.out.println("-------------------------------------");
+        System.out.println("------------------------------------");
 
-//        List<String> firstOnes = bingoPool.subList(0,15);
-        List<String> firstOnes = new ArrayList<>(bingoPool.subList(0,15));
+//        List<String> firstOnes = bingoPool.subList(0, 15);
+        List<String> firstOnes = new ArrayList<>(bingoPool.subList(0, 15));
         firstOnes.sort(Comparator.naturalOrder());
         firstOnes.replaceAll(s -> {
             if (s.indexOf('G') == 0 || s.indexOf("O") == 0) {
@@ -37,12 +36,12 @@ public class Main {
             }
             return s;
         });
-        System.out.println("\n-------------------------------------");
+        System.out.println("\n----------------------------------");
 
         for (int i = 0; i < 15; i++) {
             System.out.println(bingoPool.get(i));
         }
-        System.out.println("---------------------------------------");
+        System.out.println("------------------------------------");
 
         var tempStream = bingoPool.stream()
                 .limit(15)
@@ -52,7 +51,77 @@ public class Main {
 //                .forEach(s -> System.out.print(s + " "));
 
         tempStream.forEach(s-> System.out.print(s + " "));
-        System.out.println("\n-------------------------------------");
-        
+        System.out.println("\n----------------------------------");
+
+        String[] strings = {"One", "Two", "Three"};
+        var firstStream = Arrays.stream(strings)
+                .sorted(Comparator.reverseOrder());
+//                .forEach(System.out::println);
+
+        var secondStream = Stream.of("Six", "Five", "Four")
+                .map(String::toUpperCase);
+//                .forEach(System.out::println);
+
+        Stream.concat(secondStream, firstStream)
+                .map(s -> s.charAt(0) + " - " + s)
+                .forEach(System.out::println);
+
+        Map<Character, int[]> myMap = new LinkedHashMap<>();
+        int bingoIndex = 1;
+        for (char c : "BINGO".toCharArray()) {
+            int[] numbers = new int[15];
+            int labelNo = bingoIndex;
+            Arrays.setAll(numbers, i -> i + labelNo);
+            myMap.put(c, numbers);
+            bingoIndex += 15;
+        }
+
+        myMap.entrySet()
+                .stream()
+                .map(e -> e.getKey() + " has range: " + e.getValue()[0] + " - " +
+                        e.getValue()[e.getValue().length - 1])
+                .forEach(System.out::println);
+
+        Random random = new Random();
+        Stream.generate(() -> random.nextInt(2))
+                .limit(10)
+                .forEach(s -> System.out.print(s + " "));
+
+        System.out.println();
+        IntStream.iterate(1, n -> n + 1)
+                .filter(Main::isPrime)
+                .limit(20)
+                .forEach(s -> System.out.print(s + " "));
+
+        System.out.println();
+        IntStream.iterate(1, n -> n + 1)
+                .limit(100)
+                .filter(Main::isPrime)
+                .forEach(s -> System.out.print(s + " "));
+
+        System.out.println();
+        IntStream.iterate(1, n -> n <= 100, n -> n + 1)
+                .filter(Main::isPrime)
+                .forEach(s -> System.out.print(s + " "));
+
+        System.out.println();
+        IntStream.rangeClosed(1, 100)
+                .filter(Main::isPrime)
+                .forEach(s -> System.out.print(s + " "));
+    }
+
+    public static boolean isPrime(int wholeNumber) {
+
+        if (wholeNumber <= 2) {
+            return (wholeNumber == 2);
+        }
+
+        for (int divisor = 2; divisor < wholeNumber; divisor++) {
+            if (wholeNumber % divisor == 0) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
