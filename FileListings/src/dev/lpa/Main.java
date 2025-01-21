5ebdd6fc268fc844bfcfdbc11eb822c405300b54
Taweewattana Prhomkotara <promakotara@gmail.com>
2025-01-21 19:04:38 +0700

    Daily Updates

--- FileListings/src/dev/lpa/Main.java
+++ FileListings/src/dev/lpa/Main.java
@@ -1,6 +1,13 @@
 package dev.lpa;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.attribute.FileTime;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.stream.Stream;
 
 public class Main {
 
@@ -8,5 +15,70 @@ public class Main {
 
         Path path = Path.of("");
         System.out.println("cwd = " + path.toAbsolutePath());
+
+        try (Stream<Path> paths = Files.list(path)) {
+            paths
+                    .map(Main::listDir)
+                    .forEach(System.out::println);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("--------------------------------------------");
+
+        try (Stream<Path> paths = Files.walk(path,2)) {
+            paths
+                    .filter(Files::isRegularFile)
+                    .map(Main::listDir)
+                    .forEach(System.out::println);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("--------------------------------------------");
+
+        try (Stream<Path> paths = Files.find(path,Integer.MAX_VALUE,
+                (p, attr) -> attr.isRegularFile() && attr.size() > 300
+                )) {
+                paths
+                    .map(Main::listDir)
+                    .forEach(System.out::println);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        path = path.resolve(".idea");
+        System.out.println("===========Directory Stream==============");
+        try (var dirs = Files.newDirectoryStream(path,"*.xml")) {
+            dirs.forEach(d -> System.out.println(Main.listDir(d)));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("===========Directory Stream==============");
+        try (var dirs = Files.newDirectoryStream(path,
+                p -> p.getFileName().toString().endsWith(".xml")
+                && Files.isRegularFile(p) && Files.size(p) > 1000
+                )) {
+            dirs.forEach(d -> System.out.println(Main.listDir(d)));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static String listDir(Path path) {
+
+        try {
+            boolean isDir = Files.isDirectory(path);
+            FileTime dateField = Files.getLastModifiedTime(path);
+            LocalDateTime modDT = LocalDateTime.ofInstant(
+                    dateField.toInstant(), ZoneId.systemDefault());
+            return "%tD %tT %-5s %12s %s"
+                    .formatted(modDT, modDT, (isDir ? "<DIR>" : ""),
+                            (isDir ? "" : Files.size(path)),path);
+        } catch (IOException e) {
+            System.out.println("Whoops! Something went wrong with " + path);
+            return path.toString();
+        }
     }
 }
