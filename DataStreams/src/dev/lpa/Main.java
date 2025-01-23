37d7c434f37a2be359dc45af009a99cb474f0346
Taweewattana Prhomkotara <promakotara@gmail.com>
2025-01-23 17:19:02 +0700

    Daily Updates

--- DataStreams/src/dev/lpa/Main.java
+++ DataStreams/src/dev/lpa/Main.java
@@ -4,19 +4,27 @@ import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 class Player implements Serializable {
 
     private final static long serialVersionUID = 1L;
 
+    private final static int version = 2;
+
     private String name;
 
     private long topScore;
 
-    private List<String> collectedWeapons = new ArrayList<>();
+    private long bigScore;
 
-    public Player(String name, long topScore, List<String> collectedWeapons) {
+    private final transient long accountId;
+
+    private List<String> collectedWeapons = new LinkedList<>();
+
+    public Player(long accountId, String name, int topScore, List<String> collectedWeapons) {
+        this.accountId = accountId;
         this.name = name;
         this.topScore = topScore;
         this.collectedWeapons = collectedWeapons;
@@ -25,10 +33,37 @@ class Player implements Serializable {
     @Override
     public String toString() {
         return "Player{" +
+                "id=" + accountId + ", " +
                 "name='" + name + '\'' +
                 ", topScore=" + topScore +
                 ", collectedWeapons=" + collectedWeapons +
                 '}';
+    }
+
+    @Serial
+    @SuppressWarnings("unchecked")
+    private void readObject(ObjectInputStream stream)
+        throws IOException, ClassNotFoundException {
+//        stream.defaultReadObject();
+//        bigScore = (bigScore == 0) ? 1_000_000_000L : bigScore;
+
+        var serializedVer = stream.readInt();
+        collectedWeapons = (List<String>) stream.readObject();
+        name = stream.readUTF();
+        topScore = (serializedVer == 1) ? stream.readInt() : stream.readLong();
+
+    }
+
+    @Serial
+    private void writeObject(ObjectOutputStream stream)
+        throws IOException {
+
+        System.out.println("--> Customized Writing");
+        stream.writeInt(version);
+        stream.writeObject(collectedWeapons);
+        stream.writeUTF(name);
+        stream.writeLong(topScore);
+
     }
 }
 
@@ -40,14 +75,22 @@ public class Main {
 //        writeData(dataFile);
 //        readData(dataFile);
 
-        Player tim = new Player("Tim", 100_000_010,
+        Player tim = new Player(555,"Tim", 100_000_010,
                 List.of("knife", "machete", "pistol"));
         System.out.println(tim);
 
         Path timFile = Path.of("tim.dat");
-//        writeObject(timFile, tim);
+        writeObject(timFile, tim);
         Player reconstitudedTim = readObject(timFile);
         System.out.println(reconstitudedTim);
+
+        Player joe = new Player(556, "Joe", 75,
+                List.of("crossbow", "rifle", "pistol"));
+        Path joeFile = Path.of("joe.dat");
+        writeObject(joeFile, joe);
+        Player reconstitutedJoe = readObject(joeFile);
+        System.out.println(joe);
+        System.out.println(reconstitutedJoe);
 
     }
 
