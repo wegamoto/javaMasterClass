67cb884b2910e5aeaec0e524af5894bd39f63984
Taweewattana Prhomkotara <promakotara@gmail.com>
2025-02-13 12:52:03 +0700

    Daily Updates

---
+++ AutoboxingAndUnboxing/src/intClass.java
@@ -0,0 +1,15 @@
+class IntClass {
+    private int myValue;
+
+    public IntClass(int myValue) {
+        this.myValue = myValue;
+    }
+
+    public int getMyValue() {
+        return myValue;
+    }
+
+    public void setMyValue(int myValue) {
+        this.myValue = myValue;
+    }
+}
