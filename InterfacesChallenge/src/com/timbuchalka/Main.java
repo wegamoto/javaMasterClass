f743d27cfa736d726601332a77a6e5853a57ae66
Taweewattana Prhomkotara <promakotara@gmail.com>
2025-02-28 15:31:05 +0700

    Daily Updates

--- InterfacesChallenge/src/com/timbuchalka/Main.java
+++ InterfacesChallenge/src/com/timbuchalka/Main.java
@@ -1,3 +1,5 @@
+package com.timbuchalka;
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -23,7 +25,7 @@ public class Main {
         // simulate getting values from a file – this allows you to type as many values as your class
         // requires, and returns an ArrayList.
 
-        com.example.game.Player tim = new com.example.game.Player("Tim", 10, 15);
+        Player tim = new Player("Tim", 10, 15);
         System.out.println(tim.toString());
         saveObject(tim);
 
@@ -34,8 +36,8 @@ public class Main {
 //        loadObject(tim);
         System.out.println(tim);
 
-        com.example.game.ISaveable werewolf = new com.example.game.Monster("Werewolf", 20, 40);
-        System.out.println("Strength = " + ((com.example.game.Monster) werewolf).getStrength());
+        ISaveable werewolf = new Monster("Werewolf", 20, 40);
+        System.out.println("Strength = " + ((Monster) werewolf).getStrength());
         saveObject(werewolf);
 
     }
@@ -69,13 +71,13 @@ public class Main {
         return values;
     }
 
-    public static void saveObject(com.example.game.ISaveable objectToSave) {
+    public static void saveObject(ISaveable objectToSave) {
         for(int i=0; i<objectToSave.write().size(); i++) {
             System.out.println("Saving " + objectToSave.write().get(i) + " to storage device");
         }
     }
 
-    public static void loadObject(com.example.game.ISaveable objectToLoad) {
+    public static void loadObject(ISaveable objectToLoad) {
         ArrayList<String> values = readValues();
         objectToLoad.read(values);
     }
