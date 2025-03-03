e627dbedfc742f5caa3ce230d2be2e54478381a8
Taweewattana Prhomkotara <promakotara@gmail.com>
2025-03-03 14:04:27 +0700

    Daily Updates

--- AccessModifiers/src/com/wewe/Main.java
+++ AccessModifiers/src/com/wewe/Main.java
@@ -3,8 +3,16 @@ package com.wewe;
 public class Main {
 
     public static void main(String[] args) {
+        Account weweAccount = new Account("Wewe");
+        weweAccount.deposit(1000);
+        weweAccount.withdraw(500);
+        weweAccount.withdraw(-200);
+        weweAccount.deposit(-20);
+        weweAccount.calculateBalance();
+        weweAccount.balance = 5000;  //Access Modifiers
 
-        
-
+        System.out.println("Balance on account is " + weweAccount.getBalance());
+        weweAccount.transactions.add(4500);
+        weweAccount.calculateBalance();
     }
 }
---
+++ Collections/src/com/wewe/Main.java
@@ -0,0 +1,7 @@
+package com.wewe;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
